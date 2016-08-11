15493bd76403e1ccf60f15beea57ba855b2071b2
Miro Zurbuchen <mzurbuchen@mzurbuchen-puzzle.ep4.puzzle.ch>
2016-08-11 11:58:23 +0200

    Added Loginview

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   get '/logins', to: 'logins#index'
+  get '/posts', to: 'posts#index'
 
   resources :chat_rooms, only: [:new, :create, :show, :index]
   root 'chat_rooms#index'
