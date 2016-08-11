f7e94f33a21a5778495cde2f58a048c96b319a0b
Janiss Binder <binder@puzzle.ch>
2016-08-11 10:32:57 +0200

    implemented chat

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ActiveRecord::Base
   belongs_to :user
   belongs_to :chat_room
+  validates :body, presence: true, length: {minimum: 2, maximum: 1000}
+  after_create_commit { MessageBroadcastJob.perform_later(self) }
   
   def timestamp
     created_at.strftime('%H:%M:%S %d %B %Y')
